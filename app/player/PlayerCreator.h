5a5046a478a9b3e9bd8073d1ed49d34b7e6138a5
konrad.ryszawy <konrad2223@gmail.com>
2024-04-15 23:13:53 +0200

    PlayerCreator docstring

--- app/player/PlayerCreator.h
+++ app/player/PlayerCreator.h
@@ -8,18 +8,54 @@
 class PlayerCreator
 {
 public:
+    /**
+     * @brief Default constructor for PlayerCreator.
+     */
     PlayerCreator();
+
+    /**
+     * @brief Destructor for PlayerCreator.
+     */
     ~PlayerCreator();
 
-    void                createPlayers();
-    int                 getNumberOfPlayers();
-    void                getPlayerNames(int numPlayers);
-    void                addPlayer(const std::string& playerName);
-    void                printPlayers();
+    /**
+     * @brief Create players based on the number of players.
+     */
+    void createPlayers();
+
+    /**
+     * @brief Get the number of players.
+     * @return The number of players.
+     */
+    int getNumberOfPlayers();
+
+    /**
+     * @brief Get the names of the players.
+     * @param numPlayers The number of players.
+     */
+    void getPlayerNames(int numPlayers);
+
+    /**
+     * @brief Get the vector of players.
+     * @return The vector of players.
+     */
     std::vector<Player> getPlayers() const { return _players; }
 
+    /**
+     * @brief Add a player to the vector of players.
+     * @param playerName The name of the player to add.
+     */
+    void addPlayer(const std::string& playerName);
+
 private:
-    std::vector<Player> _players = {};
+    /**
+     * @brief Print the players' names.
+     */
+    void printPlayers();
+
+
+private:
+    std::vector<Player> _players = {}; /**< The vector of players. */
 };
 
 
