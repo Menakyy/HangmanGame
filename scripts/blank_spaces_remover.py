9ba7bfda64237815ba102bd479135e9bb608c7fb
konrad.ryszawy <konrad2223@gmail.com>
2024-04-15 23:15:41 +0200

    Scripts

--- scripts/blank_spaces_remover.py
+++ scripts/blank_spaces_remover.py
@@ -1,11 +1,11 @@
 def main() -> None:
     try:
-        with open("tests/utils/passwords.txt", "r") as file:
+        with open("app/passwordFileReader/passwords.txt", "r") as file:
             lines = file.readlines()
 
         cleaned_lines = [line.strip() for line in lines]
 
-        with open("tests/utils/passwords.txt", "w") as file:
+        with open("app/passwordFileReader/passwords.txt", "w") as file:
             file.write("\n".join(cleaned_lines))
     except IOError as e:
         print("File open error:", e)
--- scripts/chmod_for_password_txt.sh
+++ scripts/chmod_for_password_txt.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
 sudo chmod 777 app/passwordFileReader/passwords.txt
+sudo chmod 777 tests/utils/passwords.txt
