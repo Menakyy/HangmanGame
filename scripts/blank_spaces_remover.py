c831c3989db1e854d2faa873983352e80f28f838
konrad.ryszawy <konrad.ryszawy@techsterowniki.pl>
2024-04-15 15:08:21 +0200

    Added script folder

---
+++ scripts/blank_spaces_remover.py
@@ -0,0 +1,16 @@
+def main() -> None:
+    try:
+        with open("tests/utils/passwords.txt", "r") as file:
+            lines = file.readlines()
+
+        cleaned_lines = [line.strip() for line in lines]
+
+        with open("tests/utils/passwords.txt", "w") as file:
+            file.write("\n".join(cleaned_lines))
+    except IOError as e:
+        print("File open error:", e)
+
+
+if __name__ == "__main__":
+    print("Removing blank spaces from passwords.txt")
+    main()
